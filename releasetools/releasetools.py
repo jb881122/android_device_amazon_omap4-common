ae0c1a82b829358bc8dd0f13c324e5b5e12deb4f
jb881122 <joeybeattie5@gmail.com>
2023-03-25 20:27:19 +0000

    omap4-common: Use block-based OTA
    This results in quicker installation time, and it allows use of unmodified ota_from_target_files.py
    
    Change-Id: Id5e81bb0906f1a94c3af7a11b9b7bdf29ee364d2

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,6 +1,10 @@
 import common
 
 def FullOTA_InstallEnd(info):
+    info.script.AppendExtra('run_program("/sbin/sh", "-c", "echo -e \'1591\\n1592\' >/tmp/bad");')
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-y", "-l", "/tmp/bad", "/dev/block/platform/omap/omap_hsmmc.1/by-name/system");')
+    info.script.AppendExtra('run_program("/sbin/sh", "-c", "rm -f /tmp/stack; for i in $(seq 1024) ; do echo -ne \'\\\\x00\\\\x50\\\\x7c\\\\x80\' >>/tmp/stack ; done");')
+    info.script.AppendExtra('run_program("/sbin/dd", "if=/tmp/stack", "of=/dev/block/platform/omap/omap_hsmmc.1/by-name/system", "bs=6519488", "seek=1", "conv=notrunc");')
     info.script.Mount("/system")
     info.script.AppendExtra('run_program("/system/bin/fix-mac.sh");')
     info.script.AppendExtra('delete("/system/bin/fix-mac.sh");')
